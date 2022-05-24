97e0e7c53c3bd2d8f36210d40cf8310a9a248bf2
Diego Araújo <diesaraujo@gmail.com>
2022-05-24 09:00:45 -0300

    Criado game Agent

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,7 +1,13 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+  @computer_name "Robotinik"
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  def start_game(player) do
+    create_player(@computer_name, :punch, :kick, :heal)
+    |> Game.start(player)
   end
 end
