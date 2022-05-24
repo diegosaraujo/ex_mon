fb2080cf4217b73699fe2dfc3576786a1da81ca6
Diego Araújo <diesaraujo@gmail.com>
2022-05-24 15:15:37 -0300

    Criação dos ataques e danos

--- lib/game/actions.ex
+++ lib/game/actions.ex
@@ -1,5 +1,6 @@
 defmodule ExMon.Game.Actions do
   alias ExMon.Game
+  alias ExMon.Game.Actions.Attack
   def fetch_move(move) do
     Game.player()
     |> Map.get(:moves)
@@ -13,7 +14,10 @@ defmodule ExMon.Game.Actions do
   end
 
   def attack(move) do
-
+    case Game.turn() do
+      :player -> Attack.attack_opponent(:computer, move)
+      :computer -> Attack.attack_opponent(:player, move)
+    end
   end
 
 end
