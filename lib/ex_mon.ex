471e06de737f17b2cb1ca7267527a3bb7471475c
Diego Araújo <diesaraujo@gmail.com>
2022-05-24 18:16:49 -0300

    Adicionando os movimentos do computer

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -3,6 +3,7 @@ defmodule ExMon do
   alias ExMon.Game.{Status, Actions}
 
   @computer_name "Robotinik"
+  @computer_moves [:move_avg, :move_rnd, :move_heal]
 
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
@@ -15,9 +16,24 @@ defmodule ExMon do
   end
 
   def make_move(move) do
+    Game.info()
+    |> Map.get(:status)
+    |> handle_status(move)
+
     move
     |> Actions.fetch_move()
     |> do_move()
+
+    computer_move(Game.info())
+  end
+
+  defp handle_status(:game_over, _move), do: Status.print_round_message(Game.info())
+  defp handle_status(_other, move) do
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+
+    computer_move(Game.info())
   end
 
   defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
@@ -31,4 +47,10 @@ defmodule ExMon do
     Status.print_round_message(Game.info)
   end
 
+  defp computer_move(%{turn: :computer, status: :continue}) do
+    move = {:ok, Enum.random(@computer_moves)}
+    do_move(move)
+  end
+
+  defp computer_move(_), do: :ok
 end
