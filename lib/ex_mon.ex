9f69f3fbd3b315d31cb8ed37adab03addecd888c
Diego Araújo <diesaraujo@gmail.com>
2022-05-24 12:55:55 -0300

    Verificando se movimento existe e refactor struct

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,7 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
-  alias ExMon.Game.Status
+  alias ExMon.Game.{Status, Actions}
+
   @computer_name "Robotinik"
 
   def create_player(name, move_avg, move_rnd, move_heal) do
@@ -11,5 +12,9 @@ defmodule ExMon do
     create_player(@computer_name, :punch, :kick, :heal)
     |> Game.start(player)
     Status.print_round_message()
+  end
+
+  def make_move(move) do
+    Actions.fetch_move(move)
   end
 end
